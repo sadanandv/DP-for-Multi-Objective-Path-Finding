df77aba6e9bc426595bae5b8693ad82fddfdba81
Sadanand Venkataraman <60425347+sadanandv@users.noreply.github.com>
2025-01-07 11:38:47 +0530

    Delete frid directory

--- frid/cost_functions.py
+++
@@ -1,8 +0,0 @@
-class CostFunctions:
-    @staticmethod
-    def static_cost(x1, y1, x2, y2):
-        return 1  # Constant cost for all transitions
-
-    @staticmethod
-    def dynamic_cost(x1, y1, x2, y2, time_step):
-        return 1 + (time_step % 5)  # Example dynamic cost
