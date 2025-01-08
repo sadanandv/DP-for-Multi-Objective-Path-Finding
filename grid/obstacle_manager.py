50b3eae88081b0109d09e284d088e49e93a14def
Sadanand Venkataraman <60425347+sadanandv@users.noreply.github.com>
2025-01-08 14:05:57 +0530

    Update obstacle_manager.py

--- grid/obstacle_manager.py
+++ grid/obstacle_manager.py
@@ -6,19 +6,20 @@ class ObstacleManager:
         self.density = density
         self.movement = movement
 
-    def place_obstacles(self, grid):
+    def place_obstacles(self, grid, sources, destinations):
         obstacles = []
         if self.mode == "static":
             for _ in range(int(grid.rows * grid.cols * self.density)):
                 while True:
                     x, y = random.randint(0, grid.rows - 1), random.randint(0, grid.cols - 1)
-                    if not grid.is_obstacle(x, y):
+                    if not grid.is_obstacle(x, y) and (x, y) not in sources and (x, y) not in destinations:
                         obstacles.append((x, y))
                         break
         elif self.mode == "dynamic":
             for _ in range(int(grid.rows * grid.cols * self.density)):
                 x, y = random.randint(0, grid.rows - 1), random.randint(0, grid.cols - 1)
-                obstacles.append((x, y))
+                if (x, y) not in sources and (x, y) not in destinations:
+                    obstacles.append((x, y))
         return obstacles
 
     def update_dynamic_obstacles(self, obstacles, grid):
